4cea603a26f59e54d391eb0865028df46ed262df
claramm1980 <mo3326511@gmail.com>
2024-04-30 09:40:36 -0300

    Button.JS

---
+++ src/Display.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+    display: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,0.6)',
+        alignItems: 'flex-end'
+    },
+displayValue: {
+    fontSize: 60,
+    color: '#ffffff',
+    }
+})
+
+export default props =>{
+    return(
+        <View style = {styles.display}>
+    )
+}
